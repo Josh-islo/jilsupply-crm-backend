f04c17678276f53f35069946462b61e3643b0854
Josh-islo <joelissovalouembet@jilsupply.com>
2025-04-03 01:23:55 +0100

    Ajout du modèle utilisateur

---
+++ models/User.js
@@ -0,0 +1,61 @@
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Veuillez fournir un nom'],
+    trim: true,
+    maxlength: [50, 'Le nom ne peut pas dépasser 50 caractères']
+  },
+  email: {
+    type: String,
+    required: [true, 'Veuillez fournir un email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Veuillez fournir un email valide'
+    ]
+  },
+  password: {
+    type: String,
+    required: [true, 'Veuillez fournir un mot de passe'],
+    minlength: [6, 'Le mot de passe doit comporter au moins 6 caractères'],
+    select: false
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Crypter le mot de passe avant l'enregistrement
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Méthode pour signer un JWT
+UserSchema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+};
+
+// Méthode pour comparer les mots de passe
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
